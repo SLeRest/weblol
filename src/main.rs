9f3757c149084e64c20c7b8e752af16d7d140f75
SLeRest <sebastienlerest22@gmail.com>
2019-05-05 19:43:03 +0200

    oui

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+extern crate serde_json;
 mod requestlolapi;
+
 use requestlolapi::RequestLolApi;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use self::serde_json::Value;
+
+fn metadata() -> std::io::Result<serde_json::Value> {
+    let file = File::open("/home/ouralgan/lol_web/lolapi/data/lolapi.json")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    let metadata: Value = serde_json::from_str(&contents)?;
+    Ok(metadata)
+}
 
 fn main() {
-    let api_key = "RGAPI-5497e51c-81d7-4acc-8506-393bd4bb9ae9";
+    let metadata = metadata().unwrap();
 
-    let request =  RequestLolApi::new(api_key, "EUW").unwrap();
-    // error handler
+    let request =  RequestLolApi::new(metadata).unwrap();
     let summoner = request.summoner("Ouralgan").unwrap();
-    let champ_master = request.champion_masteries(&summoner.id).unwrap();
 }
--- src/requestlolapi.rs
+++ src/requestlolapi.rs
@@ -3,30 +3,8 @@ extern crate serde;
 extern crate reqwest;
 
 use self::serde::{Serialize, Deserialize};
+use self::serde_json::{Value, json};
 use std::fmt;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Summoner {
-    pub id: String,
-    pub accountId: String,
-    pub puuid: String,
-    pub name: String,
-    pub profileIconId: u64,
-    pub revisionDate: u64,
-    pub summonerLevel: u64,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ChampionMastery {
-    pub championId: u32,
-    pub championLevel: u32,
-    pub championPoints: u64,
-    pub lastPlayTime: u64,
-    pub championPointsSinceLastLevel: u64,
-    pub championPointsUntilNextLevel: u64,
-    pub chestGranted: bool,
-    pub tokensEarned: u64,
-}
 
 pub struct RequestLolApi {
     api_key: String,
@@ -35,48 +13,48 @@ pub struct RequestLolApi {
 
 impl RequestLolApi {
 
-    pub fn new(api_key: &str, region: &str) -> Result<Self, String> {
-        let uri_region = match region {
-            "BR" => "https://br1.api.riotgames.com".to_string(),
-            "EUNE" => "https://eun1.api.riotgames.com".to_string(),
-            "EUW" => "https://euw1.api.riotgames.com".to_string(),
-            "JP" => "https://jp1.api.riotgames.com".to_string(),
-            "KR" => "https://kr.api.riotgames.com".to_string(),
-            "LAN" => "https://la1.api.riotgames.com".to_string(),
-            "LAS" => "https://la2.api.riotgames.com".to_string(),
-            "NA" => "https://na1.api.riotgames.com".to_string(),
-            "OCE" => "https://oc1.api.riotgames.com".to_string(),
-            "TR" => "https://tr1.api.riotgames.com".to_string(),
-            "RU" => "https://ru.api.riotgames.com".to_string(),
-            "PBE" => "https://pbe1.api.riotgames.com".to_string(),
+    pub fn new(metadata: Value) -> Result<Self, String> {
+        let uri_region = match metadata["region"] {
+            json!("BR") => "https://br1.api.riotgames.com".to_string(),
+            json!("EUNE") => "https://eun1.api.riotgames.com".to_string(),
+            json!("EUW") => "https://euw1.api.riotgames.com".to_string(),
+            json!("JP") => "https://jp1.api.riotgames.com".to_string(),
+            json!("KR") => "https://kr.api.riotgames.com".to_string(),
+            json!("LAN") => "https://la1.api.riotgames.com".to_string(),
+            json!("LAS") => "https://la2.api.riotgames.com".to_string(),
+            json!("NA") => "https://na1.api.riotgames.com".to_string(),
+            json!("OCE") => "https://oc1.api.riotgames.com".to_string(),
+            json!("TR") => "https://tr1.api.riotgames.com".to_string(),
+            json!("RU") => "https://ru.api.riotgames.com".to_string(),
+            json!("PBE") => "https://pbe1.api.riotgames.com".to_string(),
             _ => {
-                return Err(format!("{}{}", "Wrong Region: ", region))
+                return Err(format!("{}{}", "Wrong Region: ", metadata["region"]))
             },
         };
         Ok(RequestLolApi {
-            api_key: api_key.to_string(),
+            api_key: metadata["api_key"].to_string(),
             region: uri_region,
         })
 
     }
 
     pub fn summoner(&self, summoner_name: &str)
-        -> Result<Summoner, Box<std::error::Error>>
+        -> Result<Value, Box<std::error::Error>>
     {
         let path = "/lol/summoner/v4/summoners/by-name/";
         let url = format!("{}{}{}?api_key={}",
                     self.region, path, summoner_name, self.api_key);
-        let Summoner: Summoner = reqwest::get(&url)?.json()?;
-        Ok(Summoner)
+        let summoner: Value = reqwest::get(&url)?.json()?;
+        Ok(summoner)
    }
 
     pub fn champion_masteries(&self, encrypted_summoner_id: &str)
-        -> Result<Vec<ChampionMastery>, Box<std::error::Error>>
+        -> Result<Vec<Value>, Box<std::error::Error>>
     {
         let path = "/lol/champion-mastery/v4/champion-masteries/by-summoner/";
         let url = format!("{}{}{}?api_key={}",
                     self.region, path, encrypted_summoner_id, self.api_key);
-        let champ_master: Vec<ChampionMastery> = reqwest::get(&url)?.json()?;
+        let champ_master: Vec<Value> = reqwest::get(&url)?.json()?;
         Ok(champ_master)
     }
 }
